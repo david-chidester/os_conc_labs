dd2776f96c513ee041e8e3ff797db50df9eda145
David Chidester <31967957+dchid@users.noreply.github.com>
2019-02-18 16:44:15 -0800

    Update scribblerlab.c
    Gratuitous connections and disconnections to eSpeak and scribbler robot removed. Only connect

--- scribblerlab.c
+++ scribblerlab.c
@@ -49,10 +49,9 @@ void godess();
 
 int main() // main function calls the functions for all of the songs. They all play in the sequence in which they're called.
 {
-  eSpeakConnect();
+  eSpeakConnect(); //connects to eSpeak
   eSpeakTalk ("This program plays several songs from the Legend of Zelda composed by Koji Kondo.");
-  eSpeakDisconnect(); // This ouputs the above sentence to espeak. A male or female voice is chosen at random.
-  
+  rConnect("/dev/rfcomm0"); // connect to Scribbler
     // All songs are composed by Koji Kondo
 
     // During the test run, the songs lasted aproximately 39.5 seconds not including the espeak messages.
@@ -64,20 +63,15 @@ int main() // main function calls the functions for all of the songs. They all p
   time();  // Song of Time		- lasts for approximately 5 seconds
   godess();  // Ballad of the Goddess	- lasts for approximately 10 seconds
 
-  eSpeakConnect();
   eSpeakTalk ("Program done. Thank you for listening to the zelda soundtrack composed by Koji Kondo. goodbye");
-  eSpeakDisconnect(); // The above message plays at the end of the program once all of the songs have been played.
-
+  eSpeakDisconnect(); // disconnect from eSpeak
+  rDisconnect(); // disconnect from Scribbler
   return 0; // return, indicating no errors have occurred
 } // main
 
 void lullaby() {
 // Zelda's Lullaby from The Legend of Zelda Ocarina of Time. Composer: Koji Kondo.
-  eSpeakConnect(); //connects to espeak
   eSpeakTalk ("Zelda's Lullaby composed by Koji Kondo");
-  eSpeakDisconnect();
-
-  rConnect("/dev/rfcomm0"); // connect to Scribbler
 
   rBeep(0.5,pitchB5);
   rBeep(0.25,pitchD6);
@@ -107,18 +101,13 @@ void lullaby() {
   rBeep(0.5,pitchB5);
   rBeep(0.25,pitchD6);
   rBeep(0.5,pitchA5);
-
-  rDisconnect(); // disconnect from Scribbler
 return;
 }
 
 void storms() {
 // Song of Storms from The Legend of Zelda Ocarina of Time. Composer: Koji Kondo.
-  eSpeakConnect();
   eSpeakTalk ("Song of Storms composed by Koji Kondo");
-  eSpeakDisconnect();
-
-  rConnect("/dev/rfcomm0"); // connect to Scribbler
+ 
   rBeep(0.125,pitchD5);
   rBeep(0.125,pitchF5);
   rBeep(0.5,pitchD6);
@@ -134,17 +123,12 @@ void storms() {
   rBeep(0.25,pitchE6);
   rBeep(0.25,pitchC6);
   rBeep(0.25,pitchA5);
-
-  rDisconnect(); // disconnect from Scribbler
 return;
 }
 
 void saria() // Saria's Song from the Legend of Zelda Ocarina of Time
 {
-  eSpeakConnect();
   eSpeakTalk ("Saria's song composed by Koji Kondo");
-  eSpeakDisconnect();
-  rConnect("/dev/rfcomm0"); // connect to Scribbler
 
   rBeep(0.125,pitchF5); // beep for 1 second at a frequency of 698 Hz.
   rBeep(0.125,pitchA5);
@@ -193,18 +177,12 @@ void saria() // Saria's Song from the Legend of Zelda Ocarina of Time
   rBeep(0.125,pitchG5);
   rBeep(0.125,pitchD5);
   rBeep(0.75,pitchE5);
- 
-rDisconnect(); // disconnect from Scribbler
-
   return;	
 }
 
 void time() // Song of Time from the Legend of Zelda Ocarina of Time
 {
-  eSpeakConnect();
   eSpeakTalk ("Song of Time composed by Koji Kondo");
-  eSpeakDisconnect();
-  rConnect("/dev/rfcomm0"); // connect to Scribbler
 
   rBeep(0.25,pitchA5);
   rBeep(0.5,pitchD5);
@@ -225,19 +203,12 @@ void time() // Song of Time from the Legend of Zelda Ocarina of Time
   rBeep(0.25,pitchC5);
   rBeep(0.125,pitchE5);
   rBeep(0.5,pitchD5);
- 
-  rDisconnect(); // disconnect from Scribbler
-
   return;	
 }
 
 void godess() {
 // Ballad of the Goddess from The Legend of Zelda Skyward Sword. Composer: Koji Kondo.
-  eSpeakConnect();
   eSpeakTalk ("Ballad of the Goddess composed by Koji Kondo");
-  eSpeakDisconnect();
-
-  rConnect("/dev/rfcomm0"); // connect to Scribbler
   
   rBeep(0.5,pitchB5);
   rBeep(0.25,pitchD6);
@@ -267,7 +238,5 @@ void godess() {
   rBeep(0.5,pitchB5);
   rBeep(0.25,pitchD6);
   rBeep(0.5,pitchA5);
-
-  rDisconnect(); // disconnect from Scribbler
 return;
 }
