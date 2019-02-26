f8f08d08089a4671bc1524590018b831e2d3254f
David Chidester <31967957+dchid@users.noreply.github.com>
2019-02-26 15:12:44 -0800

    First Commit (Outline for program
    A first commit for the outline of the image processing project

---
+++ image_processing.c
@@ -0,0 +1,26 @@
+int main () {
+	return 0;
+}
+
+void pixelStrip (Picture *pic, char color){
+
+}
+
+void pictureRedder(Picture * pic){
+
+}
+
+void pictureGreener (Picture * pic){
+
+}
+
+void pictureBluer (Picture * pic){
+
+}
+
+void circleSelect (Picture * pic, int xCenter, int yCenter, int radius){
+
+}
+
+
+
